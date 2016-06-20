e8d2a8f82a02b1933e65a59dc3b4f722ee017138
fatihcode <cmuratfatih@gmail.com>
2016-06-20 13:48:33 -0400

    Move DoRequest

--- search/search.go
+++ search/search.go
@@ -119,3 +119,21 @@ func ByKeyword(provider, keyword string, args map[string]string) {
 	t.PrintData()
 	fmt.Printf("\n%dms\n", int64(elapsed/time.Millisecond))
 }
+
+// DoRequest makes a HTTP request with contex
+func DoRequest(ctx context.Context, req *http.Request, f func(*http.Response, error) error) error {
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	c := make(chan error, 1)
+	go func() {
+		c <- f(client.Do(req))
+	}()
+	select {
+	case <-ctx.Done():
+		tr.CancelRequest(req)
+		<-c
+		return ctx.Err()
+	case err := <-c:
+		return err
+	}
+}
