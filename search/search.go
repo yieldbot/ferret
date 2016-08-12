0ffea65cdabdd0812b04fb02fb6f53806b3f1606
fatihcode <cmuratfatih@gmail.com>
2016-08-12 14:20:53 -0400

    Move query methods to query.go

--- search/search.go
+++ search/search.go
@@ -9,17 +9,13 @@ package search
 
 import (
 	"errors"
-	"fmt"
-	"net/http"
 	"os"
-	"os/exec"
 	"reflect"
 	"sort"
 	"strconv"
 	"time"
 
 	prov "github.com/yieldbot/ferret/providers"
-	"github.com/yieldbot/gocli"
 	"golang.org/x/net/context"
 )
 
@@ -129,109 +125,6 @@ func ProviderRegister(provider interface{}) error {
 type Searcher interface {
 	// Search makes a search
 	Search(ctx context.Context, args map[string]interface{}) ([]map[string]interface{}, error)
-}
-
-// Do runs the search query
-func (query *Query) Do() error {
-
-	// Provider
-	provider, ok := providers[query.Provider]
-	if !ok {
-		query.HTTPStatus = http.StatusBadRequest
-		return fmt.Errorf("invalid search provider. Possible search providers are %s", Providers())
-	}
-
-	// Keyword
-	if query.Keyword == "" {
-		query.HTTPStatus = http.StatusBadRequest
-		return errors.New("missing keyword")
-	}
-
-	// Page
-	if query.Page <= 0 {
-		query.HTTPStatus = http.StatusBadRequest
-		return errors.New("invalid page #. It should be greater than 0")
-	}
-
-	// Limit
-	if query.Limit <= 0 {
-		query.HTTPStatus = http.StatusBadRequest
-		return errors.New("invalid limit. It should be greater than 0")
-	}
-
-	// Search
-	query.Start = time.Now()
-	ctx, cancel := context.WithTimeout(context.Background(), query.Timeout)
-	defer cancel()
-	sq := map[string]interface{}{"page": query.Page, "limit": query.Limit, "keyword": query.Keyword}
-	sr, err := provider.Search(ctx, sq)
-	if err != nil {
-		if err == context.DeadlineExceeded {
-			query.HTTPStatus = http.StatusGatewayTimeout
-			return errors.New("timeout")
-		} else if err == context.Canceled {
-			query.HTTPStatus = http.StatusInternalServerError
-			return errors.New("canceled")
-		}
-		query.HTTPStatus = http.StatusInternalServerError
-		return errors.New("failed to search due to " + err.Error())
-	}
-	query.Elapsed = time.Since(query.Start)
-	for _, srv := range sr {
-		var d string
-		if _, ok := srv["Description"]; ok {
-			d = srv["Description"].(string)
-		}
-
-		var t time.Time
-		if _, ok := srv["Date"]; ok {
-			t = srv["Date"].(time.Time)
-		}
-
-		query.Results = append(query.Results, Result{
-			Link:        srv["Link"].(string),
-			Title:       srv["Title"].(string),
-			Description: d,
-			Date:        t,
-			From:        provider.Title,
-		})
-	}
-
-	// Goto
-	if query.Goto != 0 {
-		if query.Goto < 0 || query.Goto > len(query.Results) {
-			return fmt.Errorf("invalid result # to go. It should be between 1 and %d", len(query.Results))
-		}
-		link := query.Results[query.Goto-1].Link
-		if _, err = exec.Command(goCommand, link).Output(); err != nil {
-			return fmt.Errorf("failed to go to %s due to %s. Check FERRET_GOTO_CMD environment variable", link, err.Error())
-		}
-		return nil
-	}
-
-	return nil
-}
-
-// DoPrint handles terminal output for Do function
-func (query *Query) DoPrint(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if query.Goto == 0 {
-		t := gocli.Table{}
-		t.AddRow(1, "#", "TITLE")
-		for i, v := range query.Results {
-			ts := ""
-			if !v.Date.IsZero() {
-				ts = fmt.Sprintf(" (%d-%02d-%02d)", v.Date.Year(), v.Date.Month(), v.Date.Day())
-			}
-			t.AddRow(i+2, fmt.Sprintf("%d", i+1), fmt.Sprintf("%s%s", v.Title, ts))
-		}
-		t.PrintData()
-		fmt.Printf("\n%dms\n", int64(query.Elapsed/time.Millisecond))
-	}
 }
 
 // ParsePage parses page from a given string
