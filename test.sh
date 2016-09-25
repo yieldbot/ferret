22a3527ce2ae002ba00e78add0570e59149c8160
fatihcode <cmuratfatih@gmail.com>
2016-09-25 17:48:17 -0400

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Get required components
+go get github.com/golang/lint/golint
+go get github.com/rakyll/statik
+go generate ./assets/
+go get -t ./...
+
+# Check formatting
+OUT=`gofmt -l . | (grep -v '^vendor\/' || true)`; if [ "$OUT" ]; then echo "gofmt: $OUT"; exit 1; fi
+
+# Check linting
+OUT=`golint ./... | (grep -v '^vendor\/' || true)`; if [ "$OUT" ]; then echo "golint: $OUT"; exit 1; fi
+
+# Check suspicious constructs
+OUT=`find . -type d | grep -v -E '(^.$|^./vendor|^./.git|^./tmp|^./releases|/assets/)' | xargs go vet`; if [ "$OUT" ]; then echo "govet: $OUT"; exit 1; fi
+
+# Build
+go build .
